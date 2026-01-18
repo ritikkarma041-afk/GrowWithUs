238f8e7edd45c767c53f025111c6d0e3bc916864
ritikkarma041-afk <ritikkarma041@gmail.com>
2026-01-18 21:20:33 +0530

    Refactor database connection to use environment variables
    Updated database connection configuration to use environment variables directly.

--- growithus_api/common/database.js
+++ growithus_api/common/database.js
@@ -8,14 +8,21 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'growithusv1',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'Ritik@123',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     logging: false, // set true if you want SQL logs
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
